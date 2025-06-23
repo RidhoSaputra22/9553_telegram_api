552b946ccda6b10d5144c2ffb08cb867c820a619
ridho-code <60309031+ridho-code@users.noreply.github.com>
2025-06-23 19:57:53 +0800

    -

--- public/build/flutter_service_worker.js
+++ public/build/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "45e7f310feb0d218922cdada38365763
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "aad3cc2e133a587d7e5c0a2b6ace6395",
+"flutter_bootstrap.js": "7435eee6a833841ccc0fb31d2ae00f59",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
